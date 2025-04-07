155c59f67d79b5fa21e574a45733de7166448239
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-07 09:38:30 -0500

    Eliminacion entity

--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantEntity.java
+++
@@ -1,7 +0,0 @@
-package co.edu.udistrital.mdp.eventos.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AssistantEntity extends JpaRepository<AssistantEntity, Long>{
-    
-}
--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/OrganizerEntity.java
+++
@@ -1,7 +0,0 @@
-package co.edu.udistrital.mdp.eventos.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrganizerEntity extends JpaRepository<OrganizerEntity, Long>{
-    
-}
