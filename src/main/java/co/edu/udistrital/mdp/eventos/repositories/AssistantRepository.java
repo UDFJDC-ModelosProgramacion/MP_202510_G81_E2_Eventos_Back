84204b77b0cbd03aa3a5f4ee392dc3820d659c45
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-07 09:40:35 -0500

    Correcion Parametros

--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantRepository.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantRepository.java
@@ -2,6 +2,8 @@ package co.edu.udistrital.mdp.eventos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface AssistantRepository extends JpaRepository<AssistantRepository, Long>{
+import co.edu.udistrital.mdp.eventos.entities.AssistantEntity;
+
+public interface AssistantRepository extends JpaRepository<AssistantEntity, Long>{
     
 }
--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/OrganizerRepository.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/OrganizerRepository.java
@@ -2,6 +2,8 @@ package co.edu.udistrital.mdp.eventos.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface OrganizerRepository extends JpaRepository<OrganizerRepository, Long>{
+import co.edu.udistrital.mdp.eventos.entities.OrganizerEntity;
+
+public interface OrganizerRepository extends JpaRepository<OrganizerEntity, Long>{
     
 }
