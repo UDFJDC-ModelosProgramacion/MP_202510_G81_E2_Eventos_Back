628f634749adb6bf75ea5fcc6de8ae57e13efacb
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-07 09:37:59 -0500

    Cambio clase a Entidad a Repository

---
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantRepository.java
@@ -0,0 +1,7 @@
+package co.edu.udistrital.mdp.eventos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AssistantRepository extends JpaRepository<AssistantRepository, Long>{
+    
+}
