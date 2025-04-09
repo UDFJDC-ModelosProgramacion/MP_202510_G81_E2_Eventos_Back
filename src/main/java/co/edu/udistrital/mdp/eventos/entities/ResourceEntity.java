bb0817a10c2216062233c3de75ac2bf18d433367
Salda1308 <dsaldanag@udistrital.edu.co>
2025-04-09 09:07:36 -0500

    Asosiaciones

--- src/main/java/co/edu/udistrital/mdp/eventos/entities/ResourceEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/ResourceEntity.java
@@ -3,11 +3,17 @@ package co.edu.udistrital.mdp.eventos.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class ResourceEntity extends BaseEntity{
+    @PodamExclude
+    @ManyToOne
+    private EventEntity event;
+    
     String url;
     String type;
     
--- src/main/java/co/edu/udistrital/mdp/eventos/entities/TicketEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/TicketEntity.java
@@ -3,11 +3,17 @@ package co.edu.udistrital.mdp.eventos.entities;
 
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class TicketEntity extends BaseEntity{
+    @PodamExclude
+    @ManyToOne
+    private EventEntity event;
+
     int price;
     int remaining;
     String classification;
