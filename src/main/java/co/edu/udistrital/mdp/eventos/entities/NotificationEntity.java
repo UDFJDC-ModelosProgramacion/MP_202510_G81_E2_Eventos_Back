1cdee3b27881c357f5c568ece048d036913b95b1
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-09 18:24:41 -0500

    Creacion de Asociaciones OneToOne con Booking, Purchase

--- src/main/java/co/edu/udistrital/mdp/eventos/entities/NotificationEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/NotificationEntity.java
@@ -2,6 +2,7 @@ package co.edu.udistrital.mdp.eventos.entities;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
 import uk.co.jemos.podam.common.PodamExclude;
 
@@ -9,8 +10,15 @@ import uk.co.jemos.podam.common.PodamExclude;
 @Entity
 public class NotificationEntity extends BaseEntity {
     @PodamExclude
-    @ManyToOne
+    @OneToOne(mappedBy = "notification")
+    private BookingEntity booking;
 
+    @PodamExclude
+    @OneToOne(mappedBy = "notification")
+    private PurchaseEntity purchase;
+
+    @PodamExclude
+    @ManyToOne
     private AssistantEntity assistant;
     
     String description;
