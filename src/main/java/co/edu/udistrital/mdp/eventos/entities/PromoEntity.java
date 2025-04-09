5218bd9273b40373776daf4d063f449bfd6e654b
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-09 18:26:09 -0500

    Creacion de Asociaciones ManyToOne con Purchase

--- src/main/java/co/edu/udistrital/mdp/eventos/entities/PromoEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/PromoEntity.java
@@ -1,11 +1,17 @@
 package co.edu.udistrital.mdp.eventos.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class PromoEntity extends PurchaseEntity{
+    @PodamExclude
+    @ManyToOne
+    private PurchaseEntity purchase;
+
     Float discount;
     String description;
     String code;
