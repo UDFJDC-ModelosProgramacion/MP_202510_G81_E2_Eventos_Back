39a9cf5b30257b06ce3ba4dec53a222d5dac7ecd
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-09 09:05:25 -0500

    Creacion Asociaciones Clases Assistant, Booking, Notification

--- src/main/java/co/edu/udistrital/mdp/eventos/entities/BookingEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/BookingEntity.java
@@ -1,10 +1,17 @@
 package co.edu.udistrital.mdp.eventos.entities;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class BookingEntity extends BaseEntity {
+    @PodamExclude
+    @ManyToOne
+
+    private AssistantEntity assistant;
+
     Integer remainingSeats;
 }
