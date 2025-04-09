763e36ece5b07bae266de072b608861d741f98df
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-09 18:27:11 -0500

    Creacion de Asociaciones OneToOne con Purchase

--- src/main/java/co/edu/udistrital/mdp/eventos/entities/RefundEntity.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/entities/RefundEntity.java
@@ -3,10 +3,16 @@ package co.edu.udistrital.mdp.eventos.entities;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToOne;
 import lombok.Data;
+import uk.co.jemos.podam.common.PodamExclude;
 
 @Data
 @Entity
 public class RefundEntity extends BaseEntity{
+    @PodamExclude
+    @OneToOne(mappedBy = "refund")
+    private PurchaseEntity purchase;
+
     Date date;
 }
