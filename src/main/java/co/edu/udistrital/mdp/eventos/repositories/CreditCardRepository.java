390cf6876287a216cc74c9249616e6281eced133
Andrés Ruiz <andresruiz14502@gmail.com>
2025-04-09 09:04:20 -0500

    Entidades y repositorios de MethodOfPayment

---
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/CreditCardRepository.java
@@ -0,0 +1,9 @@
+package co.edu.udistrital.mdp.eventos.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import co.edu.udistrital.mdp.eventos.entities.CreditCardEntity;
+
+public interface CreditCardRepository extends JpaRepository<CreditCardEntity,Long>{
+
+}
