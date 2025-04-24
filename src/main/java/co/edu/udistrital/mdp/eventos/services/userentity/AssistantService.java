caed47ca1f8b004a9becfc4d1f413f38eed9cf9e
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-24 16:40:28 -0500

    Creacion de los metodos para AssistantEntity. Obtener todos los assistant obtener los datos de un assistant Actualizar informacion de un assistant Borrar un assistant(faltan validaciones)

--- src/main/java/co/edu/udistrital/mdp/eventos/services/userentity/AssistantService.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/services/userentity/AssistantService.java
@@ -1,10 +1,16 @@
 package co.edu.udistrital.mdp.eventos.services.userentity;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.edu.udistrital.mdp.eventos.exceptions.ErrorMessage;
+import co.edu.udistrital.mdp.eventos.exceptions.IllegalOperationException;
 import co.edu.udistrital.mdp.eventos.entities.userentity.AssistantEntity;
+import co.edu.udistrital.mdp.eventos.exceptions.EntityNotFoundException;
 import co.edu.udistrital.mdp.eventos.repositories.AssistantRepository;
 import lombok.extern.slf4j.Slf4j;
 
@@ -23,12 +29,78 @@ public class AssistantService {
      * @throws Exception Si ocurre un error al crear el asistente.
      */
 
+    //@Transactional
+    //public AssistantEntity createAssistant(AssistantEntity assistant) throws IllegalOperationException{
+    //    log.info("Inicia el proceso de creación de un nuevo asistente.");
+    //    
+    //    return assistantRepository.save(assistant);
+    //}
+
+
+    /*
+	 * Obtiene la lista de los registros de Assistant.
+	 *
+	 * @return Colección de objetos de AssistantEntity.
+	 */
+
     @Transactional
-    public AssistantEntity createAssistant(AssistantEntity assistant) {
-        log.info("Inicia el proceso de creación de un nuevo asistente.");
-        
-        return assistantRepository.save(assistant);
+    public List<AssistantEntity> getAllAssistants() {
+        log.info("Inicia el proceso de obtencion de todos los asistentes.");
+        return assistantRepository.findAll();
     }
 
 
+    /*
+	 * Obtiene los datos de una instancia de Assistant a partir de su ID.
+	 *
+	 * @param assistantId Identificador de la instancia a consultar
+	 * @return Instancia de AssistantEntity con los datos del Assistant consultado.
+	 */
+
+    @Transactional
+    public AssistantEntity getAssistantById(Long assistantId) throws EntityNotFoundException{
+        log.info("Inicia el proceso de obtencion de un asistente = {0}", assistantId);
+        Optional<AssistantEntity> assistantEntity = assistantRepository.findById(assistantId);
+        if(assistantEntity.isEmpty()) {
+            throw new EntityNotFoundException(ErrorMessage.ASSISTANT_NOT_FOUND);
+        }
+        log.info("Termina proceso de obtencion de un asistente = {0}", assistantId);
+        return assistantEntity.get();
+    }
+
+    /*
+	 * Actualiza la información de una instancia de Assistant.
+	 *
+	 * @param assistantId      Identificador de la instancia a actualizar
+	 * @param assitantEntity   Instancia de AuthorEntity con los nuevos datos.
+	 * @return                 Instancia de AssistantEntity con los datos actualizados.
+	 */
+
+    @Transactional
+    public AssistantEntity updateAssistant(Long assistantId, AssistantEntity assistantEntity) throws EntityNotFoundException {
+        log.info("Inicia el proceso de actualización de un asistente = {0}", assistantId);
+        Optional<AssistantEntity> assistant = assistantRepository.findById(assistantId);
+        if(assistant.isEmpty()) {
+            throw new EntityNotFoundException(ErrorMessage.ASSISTANT_NOT_FOUND);
+        }
+        assistantEntity.setId(assistantId);
+        return assistantRepository.save(assistantEntity);
+    }
+
+    /*
+	 * Elimina una instancia de Assistant de la base de datos.
+	 *
+	 * @param assitantId Identificador de la instancia a eliminar.
+	 */
+
+    @Transactional
+    public void deleteAssistant(Long assistantId) throws EntityNotFoundException, IllegalStateException {
+        log.info("Inicia el proceso de eliminación de un asistente = {0}", assistantId);
+        Optional<AssistantEntity> assistant = assistantRepository.findById(assistantId);
+        if(assistant.isEmpty()) {
+            throw new EntityNotFoundException(ErrorMessage.ASSISTANT_NOT_FOUND);
+        }
+        assistantRepository.delete(assistant.get());
+        assistantRepository.deleteById(assistantId);;
+    }
 }
