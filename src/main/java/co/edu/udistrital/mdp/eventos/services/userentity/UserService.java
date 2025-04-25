67c67410ecf5beeb1e061933a710d257c6b35f8b
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-25 14:33:24 -0500

    Clase de servicio de UserEntity

--- src/main/java/co/edu/udistrital/mdp/eventos/services/userentity/UserService.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/services/userentity/UserService.java
@@ -1,5 +1,14 @@
 package co.edu.udistrital.mdp.eventos.services.userentity;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Service
 public class UserService {
+
+    
 
 }
