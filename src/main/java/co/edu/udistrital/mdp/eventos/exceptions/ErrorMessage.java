c3896ee5641dee82db6a4d88724a19ece5cf6922
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-26 18:36:26 -0500

    Nueva condicion de error para PREFERENCE

--- src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
@@ -5,6 +5,8 @@ public class ErrorMessage {
     public static final String ASSISTANT_NOT_FOUND = "The assistant with the given id was not found";
     public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
 
+    public static final String PREFERENCE_NOT_FOUND = "The preference with the given id was not found";
+
 
     private ErrorMessage() {
         throw new IllegalStateException("Utility class");
