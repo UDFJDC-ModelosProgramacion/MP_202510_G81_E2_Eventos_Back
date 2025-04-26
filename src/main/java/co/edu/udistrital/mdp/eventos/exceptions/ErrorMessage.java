388ca34f97bab093abfb2794b5ed32da315f0c88
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-26 18:56:37 -0500

    Nueva mensaje de error para OrganizerEntity

--- src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
@@ -3,6 +3,7 @@ package co.edu.udistrital.mdp.eventos.exceptions;
 public class ErrorMessage {
     public static final String EVENT_NOT_FOUND = "EVENT_NOT_FOUND";
     public static final String ASSISTANT_NOT_FOUND = "The assistant with the given id was not found";
+    public static final String ORGANIZER_NOT_FOUND = "The organizer with the given id was not found";
     public static final String USER_NOT_FOUND = "USER_NOT_FOUND";
 
     public static final String PREFERENCE_NOT_FOUND = "The preference with the given id was not found";
