ff7acb9ce320079bda126716b68950e5a9a45c7f
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-29 11:22:37 -0500

    Implementacion metodo findByEmail

--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/OrganizerRepository.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/OrganizerRepository.java
@@ -1,9 +1,12 @@
 package co.edu.udistrital.mdp.eventos.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.edu.udistrital.mdp.eventos.entities.userentity.OrganizerEntity;
 
 public interface OrganizerRepository extends JpaRepository<OrganizerEntity, Long>{
-    
+    Optional<OrganizerEntity> findByEmail(String email);
+
 }
