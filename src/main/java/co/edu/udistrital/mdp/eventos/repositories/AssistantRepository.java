9069aba0a9243bdd5f19f4beeea5879c16e4902f
Lucho-ctrl <kipotosqui33@gmail.com>
2025-04-29 10:53:39 -0500

    Metodo para buscar un Assistant por su correo

--- src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantRepository.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/repositories/AssistantRepository.java
@@ -1,9 +1,12 @@
 package co.edu.udistrital.mdp.eventos.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import co.edu.udistrital.mdp.eventos.entities.userentity.AssistantEntity;
 
 public interface AssistantRepository extends JpaRepository<AssistantEntity, Long>{
-    
+    Optional<AssistantEntity> findByEmail(String email);
+
 }
