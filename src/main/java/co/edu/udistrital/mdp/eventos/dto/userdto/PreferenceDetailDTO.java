615dbe2c3a8aafe403a84e503ed2a79dd968bb53
Lucho-ctrl <kipotosqui33@gmail.com>
2025-05-29 09:38:30 -0500

    Relacion muchos a muchos con assistant

--- src/main/java/co/edu/udistrital/mdp/eventos/dto/userdto/PreferenceDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/dto/userdto/PreferenceDetailDTO.java
@@ -1,9 +1,12 @@
 package co.edu.udistrital.mdp.eventos.dto.userdto;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 import lombok.Data;
 
 @Data
 public class PreferenceDetailDTO extends PreferenceDTO {
-    
+    private List<AssistantDetailDTO> assistants = new ArrayList<>();
 }
