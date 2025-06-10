f7171437e1a50a5a7d68015127643409175514b8
Lucho-ctrl <kipotosqui33@gmail.com>
2025-06-10 16:19:26 -0500

    Mensajes de error para notification

--- src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/exceptions/ErrorMessage.java
@@ -12,6 +12,10 @@ public class ErrorMessage {
     public static final String BOOKING_NOT_FOUND = "The booking with the given id was not found";
     public static final String BOOKING_NOT_ASSOCIATED = "The booking is not associated to the assistant";
 
+    public static final String NOTIFICATION_NOT_FOUND = "The notification with the given id was not found";
+    public static final String NOTIFICATION_NOT_ASSOCIATED = "The notification is not associated to the assistant";
+
+
 
     private ErrorMessage() {
         throw new IllegalStateException("Utility class");
