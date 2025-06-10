8a96da73429585401402144a92fb906ddd6b99a1
Lucho-ctrl <kipotosqui33@gmail.com>
2025-06-09 20:16:06 -0500

    Implementaicon de List de relaciones OneToMany

--- src/main/java/co/edu/udistrital/mdp/eventos/dto/userdto/AssistantDetailDTO.java
+++ src/main/java/co/edu/udistrital/mdp/eventos/dto/userdto/AssistantDetailDTO.java
@@ -3,9 +3,17 @@ package co.edu.udistrital.mdp.eventos.dto.userdto;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.management.Notification;
+
+import co.edu.udistrital.mdp.eventos.dto.bookingdto.BookingDTO;
+import co.edu.udistrital.mdp.eventos.dto.bookingdto.NotificationDTO;
+import co.edu.udistrital.mdp.eventos.dto.bookingdto.PurchaseDTO;
 import lombok.Data;
 
 @Data
 public class AssistantDetailDTO extends AssistantDTO{
     private List<PreferenceDTO> preferences = new ArrayList<>();
+    private List<NotificationDTO> notifications = new ArrayList<>();
+    private List<BookingDTO> bookings = new ArrayList<>();
+    private List<PurchaseDTO> purchases = new ArrayList<>();
 }
